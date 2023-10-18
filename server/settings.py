d138ee3e3875810e0d3a98f73e9d0f9eeafcefdd
bulldak <37262325+bulldak@users.noreply.github.com>
2023-10-19 00:49:29 +0900

    CORS 추가변경
    nodejs 부분 로컬호스트로 변경하면서 같이 변경

--- server/settings.py
+++ server/settings.py
@@ -139,4 +139,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #CORS configuration
-CORS_ORIGIN_ALLOW_ALL= True
+#CORS_ORIGIN_ALLOW_ALL= True
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
+CORS_ALLOW_CREDENTIALS = True
